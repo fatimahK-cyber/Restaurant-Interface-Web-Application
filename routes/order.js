124e38a4827e08ea702dc90fdf137dc1b1b060c6
Arvin <arvin.ziba@ontariotechu.net>
2026-03-30 20:45:18 -0400

    Write middleware to fetch menu items and their IDs

--- routes/order.js
+++ routes/order.js
@@ -8,7 +8,7 @@ router.get('/', (req, res, next) => {
   res.render('order_submission', { title: 'Make an Order' });
 });
 
-router.post('/', (req, res, next) => { 
+router.post('/', (req, res, next) => {  // Finds an existing customer or creates a new one 
     console.log('Received order data: ', req.body);
     try {
         let customer = {
@@ -24,7 +24,7 @@ router.post('/', (req, res, next) => {
             if (err) {
                 console.error('Error checking customer existence: ', err);
             } else if  (results.length == 0) {
-                let insert_customer_query = `INSERT INTO customer (name, email, phone) VALUES ('${customer.name}', '${customer.email}', '${customer.phone}')`;
+                let insert_customer_query = `INSERT INTO customer (name, email, phone, address) VALUES ('${customer.name}', '${customer.email}', '${customer.phone}', '${customer.address}')`;
                 connection.query(insert_customer_query, (err, result) => {
                     if (err) {
                         console.error('Error inserting new customer: ', err);
@@ -45,12 +45,59 @@ router.post('/', (req, res, next) => {
     }
 });
 
-router.use((customer_id, req, res, next) => {
+router.use((customer_id, req, res, next) => { // Creates an order record for the customer
     console.log('Processing order for customer: ', customer_id);
-    let order = {    };
-
+    let order = {
+        customer_id: customer_id,
+        type: req.body.Type,
+        status: 'Pending',
+        creation_timestamp: new Date()
+    };
+    /*
+    connection.query('INSERT INTO `order` (Customer_ID, Type, Status, Creation_Timestamp) VALUES (?, ?, ?, ?)',
+        [order.customer_id, order.type, order.status, order.creation_timestamp],
+        (err, result) => {
+            if (err) {
+                console.error('Error inserting order: ', err);
+                return;
+            } else {
+                console.log('Inserted order with ID: ', result.insertId);
+                req.context = { order_id: result.insertId }; // Store the order ID in the request context for use in the next middleware
+                next();
+            }
+    });
+    */
+   next(1); 
 });
 
+router.use((order_id, req, res, next) => { // Gets the menu names and their IDs
+    let get_menu_items_query = 'SELECT Menu_Item_ID, Name FROM menu_item';
+
+    connection.query(get_menu_items_query, (err, result) => {
+        if (err) {
+            console.log('Error getting menu items: ', err);
+        } else {
+            console.log('Menu items: ', result); 
+            req.context = { menu_items: result, order_id: order_id }; // Store the menu items and order ID in the request context for use in the next middleware
+            next();
+        }
+    });
+});
+
+router.use((req, res, next) => { // Inserts records into the order_item table for each menu item in the order
+    let insert_order_item_query = 'INSERT INTO `order_item` (Quantity, Order_ID, Menu_Item_ID) VALUES (?, ?, ?, ?)'
+    
+    selected_item_names = Object.keys(req.body.Items); 
+    console.log('Menu item names: ', selected_item_names);
+    
+    for (const [item_name, quantity] of Object.entries(req.body.Items)) {
+        for (const menu_item of req.context.menu_items) {
+            if (menu_item.Name === item_name && quantity > 0) {
+                console.log(`Inserting ${quantity} orders of ${item_name} into order ID ${req.context.order_id}`);
+            }
+        }
+    }
+});
 
 router.get('/management', (req, res, next) => {
     res.render('order_manager', { title: 'Manage Orders' });
